88c15994a15b0f53f667d0e042a62af2cee56045
Mxyl Valtapaz <jtpavlat@earthlink.net>
2022-07-10 12:36:29 -0400

    File input JS

--- tools/scriptToLines/script.js
+++ tools/scriptToLines/script.js
@@ -1,33 +1,38 @@
-
 const characters = [
     'ACE','DANT','CAST','CASTILLES',
     'HECTOR','ELSI','BARRACKS',
     'DECK','WILLIAM','JEFF','ADELPHA','UNKNOWN',
 ];
 
-let filename = `./script.xml`
-
-function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      start(this.responseText);
-    }
-    // xhttp.setRequestHeader(``)
-    xhttp.open("GET", "script.xml", true);
-    xhttp.send();
+function loadDoc(e) {
+    console.log(e);
+    let file = e.files[0];
+    console.log(file);
+    let reader = new FileReader();
+    readSuccess=((e)=>{
+        let scriptFile = e.explicitOriginalTarget.result;
+        const parser = new DOMParser();
+        const scriptParsed = parser.parseFromString(scriptFile,"application/xml").documentElement;
+        const script = scriptParsed.getElementsByTagName(`para`);
+        start(script)
+    });
+    let item = reader.readAsText(file);
+    reader.onload=readSuccess;
+    console.log(item);
 }
 // loadDoc();
+const loadIt = (e) => {
+    console.log(e)
+}
 
-const start = (scriptFile) => {
-    const parser = new DOMParser();
-    const scriptParsed = parser.parseFromString(scriptFile,"application/xml").documentElement;
-    const script = scriptParsed.getElementsByTagName(`para`);
+const start = (script) => {
     const scriptWithoutBreaks = [];
     for (const line of script) {
         if (line.firstChild !== null && !/^\(.+\)\s*$/.test(line.firstChild.textContent)) {
             scriptWithoutBreaks.push(line);
         }
     }
+    console.log(scriptWithoutBreaks)
     const lines = [];
     for (const line of scriptWithoutBreaks) {
         if (!/^[A-Z\s:\(\)’'.-]+$/.test(line.firstChild.textContent)) {
